6e0b5ebe870db405e819c0640f03bb16227cf10f
ghanem <ghanem.lafi@external.engie.com>
2024-02-07 13:00:19 +0100

    more tests

---
+++ MobileTopup.Tests/Topups/AddTopUpBeneficiaryCommandTests.cs
@@ -0,0 +1,53 @@
+﻿using MobileTopup.Application.Topups.Commands.AddTopupBeneficiary;
+using MobileTopup.IntegrationTests;
+using Xunit;
+using Shouldly;
+
+namespace MobileTopup.Tests.Topups
+{
+    [Collection(nameof(SliceFixture))]
+    public class AddTopUpBeneficiaryCommandTests
+    {
+        private readonly SliceFixture _fixture;
+
+        public AddTopUpBeneficiaryCommandTests(SliceFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void AddTopUpBeneficiaryCommand_ValidParameters_ShouldPassValidation()
+        {
+            var command = new AddTopUpBeneficiaryCommand(Guid.NewGuid(), "Beneficiary1");
+
+            var validator = new AddTopUpBeneficiaryCommandValidator();
+
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void AddTopUpBeneficiaryCommand_InvalidUserId_ShouldFailValidation()
+        {
+
+            var command = new AddTopUpBeneficiaryCommand(Guid.Empty, "Beneficiary1");
+
+            var validator = new AddTopUpBeneficiaryCommandValidator();
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldContain(e => e.PropertyName == nameof(AddTopUpBeneficiaryCommand.UserId));
+        }
+
+        [Fact]
+        public void AddTopUpBeneficiaryCommand_ExeedNickNameLenght_ShouldFailValidation()
+        {
+
+            var command = new AddTopUpBeneficiaryCommand(Guid.NewGuid(), "Beneficiary1Beneficiary1");
+
+            var validator = new AddTopUpBeneficiaryCommandValidator();
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldContain(e => e.PropertyName == nameof(AddTopUpBeneficiaryCommand.Nickname));
+        }
+    }
+}
--- MobileTopup.Tests/Users/CreateUserCommandTests.cs
+++ MobileTopup.Tests/Users/CreateUserCommandTests.cs
@@ -4,14 +4,14 @@ using MobileTopup.IntegrationTests;
 using Xunit;
 using Shouldly;
 
-namespace MobileTopup.Tests.CreateUser
+namespace MobileTopup.Tests.Users
 {
     [Collection(nameof(SliceFixture))]
-    public class CreateTests
+    public class CreateUserCommandTests
     {
         private readonly SliceFixture _fixture;
 
-        public CreateTests(SliceFixture fixture) => _fixture = fixture;
+        public CreateUserCommandTests(SliceFixture fixture) => _fixture = fixture;
 
         [Fact]
         public async Task Should_create_user()
